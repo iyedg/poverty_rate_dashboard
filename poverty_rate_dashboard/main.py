2019d378056ffff5a55b7b73d4942c4b2bcb72ad
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2020-11-10 23:32:27 +0100

    add a simple dataviz with an OLS regression

--- poverty_rate_dashboard/main.py
+++ poverty_rate_dashboard/main.py
@@ -2,11 +2,27 @@ import pandas as pd
 import plotly.io as pio
 import streamlit as st
 from pyprojroot import here
+import plotly.express as px
 
 pio.templates.default = "plotly_white"
 
 st.set_page_config(layout="wide")
 st.header("Poverty rate dashboard")
 
+poverty_rate_df = pd.read_csv(here("data/processed/poverty_rate_Tunisia_2020.csv"))
 
-st.dataframe(pd.read_csv(here("data/processed/poverty_rate_Tunisia_2020.csv")))
+scatter_fig = px.scatter(
+    poverty_rate_df,
+    x="Abandon primaire et secondaire %",
+    y="Taux de pauvreté",
+    facet_col="Gouvernorat",
+    facet_col_wrap=4,
+    color="Gouvernorat",
+    trendline="ols",
+    height=2000,
+)
+
+st.plotly_chart(
+    scatter_fig,
+    use_container_width=True,
+)
